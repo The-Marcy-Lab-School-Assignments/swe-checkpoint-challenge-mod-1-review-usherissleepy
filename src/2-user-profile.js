b969e33a9abed88a39803e176739140e06a5ebbd
[usherissleepy] <[ashar.zarrar218@gmail.com]>
2025-12-11 14:02:49 -0500

    finished

--- src/2-user-profile.js
+++ src/2-user-profile.js
@@ -1,26 +1,35 @@
 // Return a new user object with username, email, isActive: true, loginCount: 0
 const createUser = (username, email) => {
-
+  return{
+    username: username,
+    email: email,
+    isActive: true,
+    loginCount: 0
+  }
 };
 
 // Increase the user's loginCount by 1
 const incrementLogin = (user) => {
-
+  user.loginCount++
 };
 
 // Set isActive to false and delete the email property. Return the user.
 const deactivateUser = (user) => {
-
+  user.isActive = false
+  delete user.email
+  return user
 };
 
 // Print each property and value in the format "key: value"
 const printUserInfo = (user) => {
-
+  for (const key in user){
+    console.log(`${key}: ${user[key]}`)
+  }
 };
 
 // BONUS: Return a true copy of the user object (not a reference)
 const cloneUser = (user) => {
-
+  return {...user}
 };
 
 module.exports = {
